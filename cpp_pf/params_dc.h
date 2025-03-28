2b1610d932ebb4352ed1ffbff7a1e52f40c0a938
ooone <160036988+HLi199305-ee@users.noreply.github.com>
2025-03-28 17:06:07 +0100

    Update params_dc.h
    Modify inconsistent arguments

--- cpp_pf/params_dc.h
+++ cpp_pf/params_dc.h
@@ -7,42 +7,18 @@
 #include <Eigen/Sparse>
 #include <complex>
 
-/* PURPOSE: DEFINE DC NETWORK PARAMETERS */
-
-//        nbuses_dc   \
-//        nbranches_dc| -- > parameters linked with number
-//        nconvs_dc   /
-//        ---------------------------------------------------
-//        rtf_dc  \
-//        xtf_dc  |
-//        bf_dc   |
-//        rc_dc   |
-//        xc_dc   | -- > parameters linked with vsc impedance
-//        ztfc_dc |
-//        gtfc_dc |
-//        btfc_dc /
-//        ----------------------------------------------------
-//        convState_dc -- > converter state : inv. or rec.
-//        ------------------------------------------------
-//        aloss_dc \
-//        bloss_dc | -- > parameters linked with converter power loss
-//        closs_dc /
-//        ------------------------------------------------------------
-//        Y_dc, y_dc -- > paramters linked with dc admittance matrix
-//        ----------------------------------------------------------------
-
 struct DCNetworkParams {
 	std::unordered_map<std::string, Eigen::MatrixXd> network_dc;
 	double baseMW_dc, pol_dc;
-	int nbuses_dc, nbranches_dc, nconvs;
-	Eigen::MatrixXd bus_dc, branch_dc, conv;
+	int nbuses_dc, nbranches_dc, nconvs_dc;
+	Eigen::MatrixXd bus_dc, branch_dc, conv_dc;
 	Eigen::SparseMatrix<std::complex<double>> Y_dc;
 	Eigen::SparseMatrix<double> y_dc;
-	Eigen::VectorXcd ztfc;
-	Eigen::VectorXd rtf, xtf, bf, rec, xc;
-	Eigen::VectorXd gtfc, btfc, aloss, bloss, closs;
+	Eigen::VectorXcd ztfc_dc;
+	Eigen::VectorXd rtf_dc, xtf_dc, bf_dc, rc_dc, xc_dc;
+	Eigen::VectorXd gtfc_dc, btfc_dc, aloss_dc, bloss_dc, closs_dc;
 	Eigen::VectorXd basekV_dc;
-	Eigen::VectorXi convState;
+	Eigen::VectorXi convState_dc;
 	Eigen::VectorXi fbus_dc, tbus_dc;
 };
 

4e93194ef966f3cb893350d9270ae3a8acc17168
ooone <160036988+HLi199305-ee@users.noreply.github.com>
2025-04-14 09:26:17 +0200

    Update main.jl
    add: include("viz_opf.jl")

--- Julia_PF/main.jl
+++ Julia_PF/main.jl
@@ -6,11 +6,12 @@
 # [3]  Matacdc1.0 unser's manual, https://www.esat.kuleuven.be/electa/teaching/matacdc/MatACDCManual
 
 cd(dirname(@__FILE__))
-include("create_ac.jl") # define interconnected ac grids
-include("create_dc.jl") # define multi-terminal dc grid
-include("makeYbus.jl") # calculate Bus Admittance Matrix
-include("params_dc.jl") # obtain ac grid parameters 
-include("params_ac.jl") # obtain dc grid parameters 
-include("solve_opf.jl") # solve ac/dc OPF
+include("create_ac.jl") 
+include("create_dc.jl")
+include("makeYbus.jl") 
+include("params_dc.jl") 
+include("params_ac.jl") 
+include("solve_opf.jl")
+include("viz_opf.jl") 
 
 result_opf = solve_opf("ac14ac57", "mtdc3slack_a")

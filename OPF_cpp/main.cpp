ea1c1e557fef255f4459174d9fb6be784fe7a630
HLi199305-ee <haixiaoli.ee@gmail.com>
2025-07-23 21:35:17 +0200

    Updates -main file

--- OPF_cpp/main.cpp
+++ OPF_cpp/main.cpp
@@ -23,10 +23,10 @@
 #include "benchmarkTools.h"
 #include "solve_opf.h"
 int main() {
-    solve_opf("mtdc3slack_a", "ac9ac14",
+    solve_opf("mtdc3slack_a", "ac14ac57",
         /*vscControl*/ true,
         /*writeTxt  */ false,
-        /*plotResult*/ true);
+        /*plotResult*/ false);
 
    /* std::vector<std::string> test_cases = { "ac9ac14" };
     std::string dc_case = "mtdc3slack_a";
